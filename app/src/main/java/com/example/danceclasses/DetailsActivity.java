332642ffa7610ce839644cb6724c1152313a4914
Sasha Chernik <alexandra.chernik@gmail.com>
2020-10-05 12:23:39 -0400

    Populated DetailsActivity.java

--- app/src/main/java/com/example/danceclasses/DetailsActivity.java
+++ app/src/main/java/com/example/danceclasses/DetailsActivity.java
@@ -1,4 +1,47 @@
 package com.example.danceclasses;
 
-public class DetailsActivity {
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private static final String TAG = "DetailsActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_layout);
+        Log.d(TAG, "onCreate: started");
+
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent() {
+        Log.d(TAG, "getincomingIntent: checking for incoming intents.");
+
+        if (getIntent().hasExtra("title") && getIntent().hasExtra("long_description")) {
+            Log.d(TAG, "getIncomingIntent: found intent extras.");
+
+            String title = getIntent().getStringExtra("title");
+            String long_description = getIntent().getStringExtra("long_description");
+
+            setInfo(title, long_description);
+        }
+    }
+
+    private void setInfo(String title, String long_description) {
+        Log.d(TAG, "setInfo: setting the title and long description to widgets.");
+
+        TextView detTitle = findViewById(R.id.details_title);
+        detTitle.setText(title);
+
+        TextView detDesc = findViewById(R.id.details_description);
+        detDesc.setText(long_description);
+
+    }
+
 }
