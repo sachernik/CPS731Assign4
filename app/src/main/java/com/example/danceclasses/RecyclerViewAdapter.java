a5dda13178093bfb28dca3d5a27468c7209b4f1b
Sasha Chernik <alexandra.chernik@gmail.com>
2020-10-05 12:42:31 -0400

    DetailsActivity functional, with placeholder text

--- app/src/main/java/com/example/danceclasses/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/danceclasses/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.danceclasses;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     private String[] Titles = new String[10];
     private String[] Descriptions = new String[10];
+
+    String[] LongDescs = new String[]{
+            "placeholder1",
+            "placeholder2",
+            "placeholder3",
+            "placeholder4",
+            "placeholder5",
+            "placeholder6",
+            "placeholder7",
+            "placeholder8",
+            "placeholder9",
+            "placeholder10",
+    };
+
     private Context mcontext;
 
     public RecyclerViewAdapter(Context mcontext, String[] TitlesArray, String[] DescriptionsArray) {
@@ -33,10 +48,23 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called");
         holder.title.setText(Titles[position]);
         holder.description.setText(Descriptions[position]);
+
+        holder.listLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Log.d(TAG,
+                // Toast.makeText.... ?
+
+                Intent intent = new Intent(mcontext, DetailsActivity.class);
+                intent.putExtra("title", Titles[position]);
+                intent.putExtra("long_description", LongDescs[position]);
+                mcontext.startActivity(intent);
+            }
+        });
     }
 
     @Override
