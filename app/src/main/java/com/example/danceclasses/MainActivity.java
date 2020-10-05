d238a813c96fc0d3b05ec832f12dd1838e9950da
Sasha Chernik <alexandra.chernik@gmail.com>
2020-10-05 11:32:19 -0400

    Added RecyclerViewAdapter

--- app/src/main/java/com/example/danceclasses/MainActivity.java
+++ app/src/main/java/com/example/danceclasses/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.danceclasses;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView rv = findViewById(R.id.recyclerview);
+        rv.setAdapter(new RecyclerViewAdapter(this));
+        rv.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/danceclasses/RecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.example.danceclasses;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<ReyclerViewAdapter.ViewHolder> {
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private String[] Titles = new String[6];
+    private String[] Descriptions = new String[6];
+    private Context mcontext;
+
+    //TextView title;
+    //TextView description;
+
+    public RecyclerViewAdapter(Context mcontext, String[] TitlesArray, String[] DescriptionsArray) {
+        this.Titles = TitlesArray;
+        this.Descriptions = DescriptionsArray;
+        this.mcontext = mcontext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem_layout,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void OnBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+        holder.title.setText(Titles[position]);
+        holder.description.setText(Descriptions[position]);
+    }
+
+    @Override
+    public int getItemCount() { return Titles.length; }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView description;
+        LinearLayout listLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.listTitle);
+            description = itemView.findViewById(R.id.listDescription);
+            listLayout = itemView.findViewById(R.id.listItemLayout);
+        }
+    }
+}
