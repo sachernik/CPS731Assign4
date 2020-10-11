6d8117a49bc0216680eafb6f7d2c7d919b718b76
Sasha Chernik <alexandra.chernik@gmail.com>
2020-10-11 11:24:30 -0400

    Added splash

---
+++ app/src/main/java/com/example/danceclasses/Splash.java
@@ -0,0 +1,27 @@
+package com.example.danceclasses;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(Splash.this, MainActivity.class));
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task, 5000);
+    }
+}
