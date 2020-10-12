567fd8d63208e04718bfec7bbc576add8bfed025
Sasha Chernik <alexandra.chernik@gmail.com>
2020-10-12 12:25:10 -0400

    Added audio to splash

--- app/src/main/java/com/example/danceclasses/Splash.java
+++ app/src/main/java/com/example/danceclasses/Splash.java
@@ -1,6 +1,7 @@
 package com.example.danceclasses;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,15 @@ import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
+    MediaPlayer md;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        md = new MediaPlayer();
+        md = MediaPlayer.create(this,R.raw.jazz);
+        md.start();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
