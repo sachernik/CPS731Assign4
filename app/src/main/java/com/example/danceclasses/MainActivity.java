7542fb288758209d0d26100fd42fbbcc6d4e9cce
Sasha Chernik <alexandra.chernik@gmail.com>
2020-10-12 13:04:49 -0400

    Modified activity_main.xml to prep for Master/Detail template, added layout files and activities

--- app/src/main/java/com/example/danceclasses/MainActivity.java
+++ app/src/main/java/com/example/danceclasses/MainActivity.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    private boolean mTwopane;
+
     Context context;
     String[] Titles = new String[10];
     String[] Descriptions = new String[10];
@@ -18,11 +20,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         context = getApplicationContext();
         this.Titles = context.getResources().getStringArray(R.array.titles_array);
         this.Descriptions = context.getResources().getStringArray(R.array.descs_array);
         this.LongDescs = context.getResources().getStringArray(R.array.longdescs_array);
-        setContentView(R.layout.activity_main);
+
         RecyclerView rv = findViewById(R.id.recyclerview);
         rv.setAdapter(new RecyclerViewAdapter(this,Titles,Descriptions,LongDescs));
         rv.setLayoutManager(new LinearLayoutManager(this));
